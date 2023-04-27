9befa63aac460dc60b88a23f3bf67b042c8d084a
Saul Ceja <saulceja08@gmail.com>
2023-04-26 22:57:10 -0700

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//add DOMContentLoaded to signal when all element in DOM are fully loaded
+document.addEventListener('DOMContentLoaded',function() => {
+  //add an addEventListener functionS
+  addingEventListeners()
+})
+
+function addingEventListeners() {
+  //get the form and add an eventListener
+  document.getElementById('create-task-form')
+    .addEventListener('submit', handleFormSubmit)
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+
+  const task = e.target[0].value
+  displayTask(task)
+}
+
+function displayTask(task) {
+  const taskUl = document.getElementById('task')
+  const taskLi = document.getElementById('li')
+
+  taskLi.textContent = tasktaskUl.appendChild(taskLi)
+}
